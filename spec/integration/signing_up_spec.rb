74eef7a2b004863e6444001085284b6dab24baba
Mike Hatfield <oakraven13@gmail.com>
2013-02-17 10:19:52 -0400

    Added feature for signing in via the Devise-provided form

--- spec/integration/signing_up_spec.rb
+++ spec/integration/signing_up_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 feature "Signing Up" do
   scenario "Successful sign up" do
     visit "/"
-    click_link "Sign Up"
+    click_link "Sign up"
     fill_in "Email", with: "user@ticketee.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
